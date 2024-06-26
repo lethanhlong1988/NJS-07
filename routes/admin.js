29f47af7f15f0f6b29803b0e5edad24dd082332a
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-06-26 05:04:27 +0000

    update

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 const express = require("express");
 
-// const rootDir = require("../util/path");
 const productsController = require("../controllers/products");
 
 const router = express.Router();
@@ -13,4 +12,4 @@ router.get("/add-product", productsController.getAddProduct);
 // /admin/add-product => POST
 router.post("/add-product", productsController.postAddProduct);
 
-exports.routes = router;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,6 @@ const express = require("express");
 
 const productsController = require("../controllers/products");
 
-const rootDir = require("../util/path");
-const adminData = require("./admin");
-
 const router = express.Router();
 
 router.get("/", productsController.getProducts);
