0ef6434d8917d35f26ab7c44f26d8bdaccf32b4a
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-06-27 04:10:10 +0000

    update

--- models/product.js
+++ models/product.js
@@ -1,4 +1,11 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+let products = [];
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +13,24 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    fs.readFile(p, (err, fileContent) => {
+      const productsData = JSON.parse(fileContent);
+      console.log(this);
+      productsData.push(this);
+      products = productsData;
+    });
+    fs.writeFile(p, JSON.stringify(products), (err) => {
+      console.log(err);
+    });
   }
 
   static fetchAll() {
+    fs.readFile(p, (err, fileContent) => {
+      const productsData = JSON.parse(fileContent);
+      console.log(this);
+      productsData.push(this);
+      products = productsData;
+    });
     return products;
   }
 };
